c3649c480de6ca1240f2b4dcf0deb56cf9fb5b9c
insach67 <insach67@gmail.com>
2022-05-04 21:57:25 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h3>Hello sir,</h3>
+
+<p>Thanks for giving me this opportunity to introduce myself, in front of you.</p>
+
+<p>My name is sachin</p> 
+<p>I am 23 years old</p>
+<p> Basically, I am from Bihar but  currently staying in Delhi.</p>
+
+<p>Coming to my family we are 5 members including me, my father, my mother, and two younger brother, my father is a farmer and my mother is a Government employee.</p>
+
+<p>Talking about my education I have completed my engineering  in electronics and communications from kuk.</p>
+
+<p>My strength is self-motivated, hardworking.</p>
+
+<p>My weakness is trusting all.</p>
+
+<p>My hobbies are  watching  and playing cricket.</p>
+
+<p>That's all about myself.</p>
+
+<p>Thank you.</p>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
